2bbc09bbd6523d17f4dbcfbc479a3d62b7812c40
adriangarciaesc <adrgaresc3@alu.edu.gva.es>
2026-03-08 16:53:04 +0100

    Version Inicial del Proyecto

---
+++ T4_P3_garcia_adrian/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace T4_P3_garcia_adrian
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxTitulo = new System.Windows.Forms.TextBox();
+            this.textBoxDescripcion = new System.Windows.Forms.TextBox();
+            this.dateTimePickerFecha = new System.Windows.Forms.DateTimePicker();
+            this.btnAñadir = new System.Windows.Forms.Button();
+            this.listBoxTareas = new System.Windows.Forms.ListBox();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnCompletar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBoxTitulo
+            // 
+            this.textBoxTitulo.Location = new System.Drawing.Point(90, 39);
+            this.textBoxTitulo.Name = "textBoxTitulo";
+            this.textBoxTitulo.Size = new System.Drawing.Size(253, 22);
+            this.textBoxTitulo.TabIndex = 0;
+            // 
+            // textBoxDescripcion
+            // 
+            this.textBoxDescripcion.Location = new System.Drawing.Point(90, 67);
+            this.textBoxDescripcion.Multiline = true;
+            this.textBoxDescripcion.Name = "textBoxDescripcion";
+            this.textBoxDescripcion.Size = new System.Drawing.Size(253, 228);
+            this.textBoxDescripcion.TabIndex = 1;
+            // 
+            // dateTimePickerFecha
+            // 
+            this.dateTimePickerFecha.Location = new System.Drawing.Point(90, 309);
+            this.dateTimePickerFecha.Name = "dateTimePickerFecha";
+            this.dateTimePickerFecha.Size = new System.Drawing.Size(253, 22);
+            this.dateTimePickerFecha.TabIndex = 2;
+            // 
+            // btnAñadir
+            // 
+            this.btnAñadir.Location = new System.Drawing.Point(129, 339);
+            this.btnAñadir.Name = "btnAñadir";
+            this.btnAñadir.Size = new System.Drawing.Size(165, 46);
+            this.btnAñadir.TabIndex = 3;
+            this.btnAñadir.Text = "Añadir Tarea";
+            this.btnAñadir.UseVisualStyleBackColor = true;
+            this.btnAñadir.Click += new System.EventHandler(this.btnAñadir_Click);
+            // 
+            // listBoxTareas
+            // 
+            this.listBoxTareas.FormattingEnabled = true;
+            this.listBoxTareas.ItemHeight = 16;
+            this.listBoxTareas.Location = new System.Drawing.Point(390, 39);
+            this.listBoxTareas.Name = "listBoxTareas";
+            this.listBoxTareas.Size = new System.Drawing.Size(381, 292);
+            this.listBoxTareas.TabIndex = 4;
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnEliminar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnEliminar.Location = new System.Drawing.Point(653, 339);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(118, 46);
+            this.btnEliminar.TabIndex = 5;
+            this.btnEliminar.Text = "Eliminar Tarea";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnCompletar
+            // 
+            this.btnCompletar.Location = new System.Drawing.Point(390, 339);
+            this.btnCompletar.Name = "btnCompletar";
+            this.btnCompletar.Size = new System.Drawing.Size(162, 43);
+            this.btnCompletar.TabIndex = 6;
+            this.btnCompletar.Text = "Marcar como completada";
+            this.btnCompletar.UseVisualStyleBackColor = true;
+            this.btnCompletar.Click += new System.EventHandler(this.btnCompletar_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Titulo:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(2, 70);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Descripcion:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCompletar);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.listBoxTareas);
+            this.Controls.Add(this.btnAñadir);
+            this.Controls.Add(this.dateTimePickerFecha);
+            this.Controls.Add(this.textBoxDescripcion);
+            this.Controls.Add(this.textBoxTitulo);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxTitulo;
+        private System.Windows.Forms.TextBox textBoxDescripcion;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFecha;
+        private System.Windows.Forms.Button btnAñadir;
+        private System.Windows.Forms.ListBox listBoxTareas;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnCompletar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ T4_P3_garcia_adrian/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace T4_P3_garcia_adrian
+{
+    public partial class Form1 : Form
+    {
+
+        GestorTareas gestor = new GestorTareas();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAñadir_Click(object sender, EventArgs e)
+        {
+            string titulo = textBoxTitulo.Text;
+            string descripcion = textBoxDescripcion.Text;
+            DateTime fecha = dateTimePickerFecha.Value;
+
+            if (titulo == "")
+            {
+                MessageBox.Show("Introduce un título");
+                return;
+            }
+
+            Tarea nueva = new Tarea(titulo, descripcion, fecha);
+            gestor.AñadirTarea(nueva);
+
+            listBoxTareas.Items.Add(nueva);
+
+            textBoxTitulo.Clear();
+            textBoxDescripcion.Clear();
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            int indice = listBoxTareas.SelectedIndex;
+
+
+            if (indice == -1)
+            {
+                MessageBox.Show("Selecciona una tarea");
+                return;
+            }
+
+
+            gestor.EliminarTarea(indice);
+            listBoxTareas.Items.RemoveAt(indice);
+
+        }
+
+        private void btnCompletar_Click(object sender, EventArgs e)
+        {
+            int indice = listBoxTareas.SelectedIndex;
+
+            if (indice == -1)
+            {
+                MessageBox.Show("Selecciona una tarea");
+                return;
+            }
+
+            gestor.MarcarCompletada(indice);
+            listBoxTareas.Items[indice] = gestor.ObtenerTareas()[indice];
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
+namespace T4_P3_garcia_adrian
+{
+    internal class Tarea
+    {
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public bool Completada { get; set; }
+        public DateTime Fecha { get; set; }
+
+        public Tarea(string titulo, string descripcion, DateTime fecha)
+        {
+            Titulo = titulo;
+            Descripcion = descripcion;
+            Fecha = fecha;
+            Completada = false;
+        }
+
+        public override string ToString()
+        {
+            string estado = Completada ? "Completada" : "No completada";
+            return $"{Titulo} - {Descripcion} - {estado} - {Fecha.ToShortDateString()}";
+        }
+    }
+}
+
+
+namespace T4_P3_garcia_adrian
+{
+    internal class GestorTareas
+    {
+        private List<Tarea> tareas;
+
+        public GestorTareas()
+        {
+            tareas = new List<Tarea>();
+        }
+
+        public void AñadirTarea(Tarea t)
+        {
+            tareas.Add(t);
+        }
+
+        public void EliminarTarea(int indice)
+        {
+            if (indice >= 0 && indice < tareas.Count)
+                tareas.RemoveAt(indice);
+        }
+
+        public void MarcarCompletada(int indice)
+        {
+            if (indice >= 0 && indice < tareas.Count)
+                tareas[indice].Completada = true;
+        }
+
+        public List<Tarea> ObtenerTareas()
+        {
+            return tareas;
+        }
+    }
+}
+
